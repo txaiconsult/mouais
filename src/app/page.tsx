c6767f51b43d2f385217376fcb7743308196d49a
Thomas Faivre <tmp.faivre@gmail.com>
2025-09-23 05:32:00 +0000

    c'est une sorte de fake loading de la page pour un effet wow avant meme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,7 @@ import SchedulerPage from "@/components/scheduler-page";
 
 // Making this component async allows it to await the async SchedulerPage
 export default async function Home() {
+  // Artificial delay to ensure the loading animation is visible
+  await new Promise(resolve => setTimeout(resolve, 2000));
   return <SchedulerPage />;
 }
